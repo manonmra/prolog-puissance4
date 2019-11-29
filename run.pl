bbb5570c1cc2a5f076281cdd5e03d56230655fdf
manonmra <48408820+manonmra@users.noreply.github.com>
2019-11-29 10:47:57 +0100

    Add files via upload

--- run.pl
+++ run.pl
@@ -1,6 +1,8 @@
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Initialisation et lancement du jeu %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Tableau initial
-initialiser(grille([
+initialiser([
 		   ['_','_','_','_','_','_'],   
 	       ['_','_','_','_','_','_'],  
 	       ['_','_','_','_','_','_'],
@@ -8,18 +10,47 @@ initialiser(grille([
 	       ['_','_','_','_','_','_'],
 	       ['_','_','_','_','_','_'],
 	       ['_','_','_','_','_','_'] 
-])).
-
+]).
+initialiser2([
+		   ['_','_','O','X','X','X'],   
+	       ['_','_','_','_','_','X'],  
+	       ['_','_','_','_','O','X'],
+	       ['_','_','_','_','O','X'],
+	       ['_','_','_','_','_','O'],
+	       ['_','_','_','_','_','_'],
+	       ['_','_','_','_','_','_'] 
+]).
 
 % Dans la grille du dessus, chaque ligne correspond à une colonne du jeu de puissance4.
 % la casse de coordonnée (1;1) dans la grille ci-dessus correspond à la case "tout en haut à droite" du jeu de puissance4
 % La casse de coordonnée (6;7) dans la grille ci-dessus correspond à la case "tout en bas  à gauche" du jeu de puissance4
 
-puissance4 :- initialiser(X), afficher(X), jouerCoup('X',X).
+puissance4 :- initialiser(G), afficher(G), nl, jouerCoup('X', G).
 
+puissance2 :- initialiser2(G), afficher(G), nl, victoire('O',G).
 
-% Afficher la grille
-afficher(grille(G)):- afficherLignes(G,6).
+%%%%%%%%%%%%%%%%%%%%%%%
+%% Mécaniques du jeu %%
+%%%%%%%%%%%%%%%%%%%%%%%
+joueurSuivant('X','O').
+joueurSuivant('O','X').
+
+remplie(G):- append(G,X), \+ member('_', X).
+
+gameOver(G) :- victoire(X,G),write('Victoire du joueur '), write(X), !.
+gameOver(G) :- remplie(G), write('Match nul, la grille est pleine!'), !.
+
+jouerCoup(_,G):- gameOver(G).
+jouerCoup(Joueur,G) :- write('Au tour de '), write(Joueur), write(' de jouer.'),
+            lireColonne(C),
+            ajouterDansColonne(Joueur, C, G, G2),
+            joueurSuivant(Joueur, JoueurSuivant), %changer de joueur
+            jouerCoup(JoueurSuivant, G2). %faire jouer le joueur suivant
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%% Afficher la grille %%
+%%%%%%%%%%%%%%%%%%%%%%%%
+afficher(G):- afficherLignes(G,6).
 
 afficherLignes(_,0).
 afficherLignes(G,N):- afficherLigne(G,G2), nl, N2 is N-1, afficherLignes(G2,N2).
@@ -32,37 +63,27 @@ afficherLignes(G,N):- afficherLigne(G,G2), nl, N2 is N-1, afficherLignes(G2,N2).
 afficherLigne([],_).
 afficherLigne([[X|Y]|Z],[Y|Z2]):- write(X), write(' '), afficherLigne(Z,Z2).
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Ajouter un coup (jeton) dans une colonne précise %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Fonction pour ajouter dans une colonne précise
+ajouterDansColonne(X,1,[Y|Z],[Y2|Z]):- ajouterAuDessus(X,Y,Y2).
+ajouterDansColonne(X,N,[Y|Z],[Y|T]):- N2 is N-1, ajouterDansColonne(X,N2,Z,T).
 
 % Fonction pour ajouter au dessus du dernier pion
 ajouterAuDessus(X,['_'],[X]). % si la colonne est vide, on ajoute à la fin
 ajouterAuDessus(X,['_',H|Q],[X,H|Q]):- H \== ('_'), !. % si l'on souhaite jouer au dessus de quelqu'un
 ajouterAuDessus(X,['_'|Q],['_'|Q2]):- ajouterAuDessus(X,Q,Q2). % utilisé pour descendre dans la colonne
 
-
-%%Jouer coups
-joueurSuivant('X','O').
-joueurSuivant('O','X').
-
-
-%Pour pouvoir jouer un coup, il faut que la grille ne soit pas remplie
-remplie(G):- append(G,X), \+ member('_', X).
-
-
-%%cas où le jeu s'arrête 
-gameOver(grille(G)) :- victoire(X,G),write('Victoire du joueur '), write(X),!. 
-gameOver(grille(G)) :- remplie(G), write('Match nul, la grille est pleine!'),!.
-
-
-
-
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Vérification partie finie (gagnant) %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Calcul de quand le joueur peut gagner
 %4 cas pour gagner
 %CAS N°1 = Le joueur X gagne car il a passé 4 pions à la verticale
-victoire(X,grille(G)):- append(_, [H|_], G), append(_,[X,X,X,X|_],H). 
+victoire(X,G):- append(_, [H|_], G), append(_,[X,X,X,X|_],H). 
 %CAS N°2 = Le joueur X gagne car il a passé 4 pions à l'horizontal
-victoire(X,grille(G)):- append(_,[C1,C2,C3,C4|_],G), %4 colonnes adjacentes
+victoire(X,G):- append(_,[C1,C2,C3,C4|_],G), %4 colonnes adjacentes
            append(I1,[X|_],C1),
            append(I2,[X|_],C2),
 		   append(I3,[X|_],C3),
@@ -70,7 +91,7 @@ victoire(X,grille(G)):- append(_,[C1,C2,C3,C4|_],G), %4 colonnes adjacentes
 		   length(I1,L1), length(I2,L2), length(I3,L3), length(I4,L4),
            L1 is L2, L1 is L3, L1 is L4.
 %CAS N°3 = Le joueur X gagne car il a crée une diagonale de gauche (haut) à droite (bas)
-victoire(X,grille(G)):- append(_,[C1,C2,C3,C4|_],G), %4 colonnes adjacentes
+victoire(X,G):- append(_,[C1,C2,C3,C4|_],G), %4 colonnes adjacentes
 		   append(I1,[X|_],C1),
 		   append(I2,[X|_],C2),
 		   append(I3,[X|_],C3),
@@ -78,7 +99,7 @@ victoire(X,grille(G)):- append(_,[C1,C2,C3,C4|_],G), %4 colonnes adjacentes
 		   length(I1,L1), length(I2,L2), length(I3,L3), length(I4,L4),
 		   L2 is L1-1, L3 is L2-1, L4 is L3-1. 
 %CAS N°4 = Le joueur X gagne car il a créé une diagonale de gauche (bas) à droite (haut)
-victoire(X,grille(G)):- append(_,[C1,C2,C3,C4|_],G), %4 colonnes adjacentes
+victoire(X,G):- append(_,[C1,C2,C3,C4|_],G), %4 colonnes adjacentes
 		   append(I1,[X|_],C1),
            append(I2,[X|_],C2),
 		   append(I3,[X|_],C3),
@@ -86,5 +107,19 @@ victoire(X,grille(G)):- append(_,[C1,C2,C3,C4|_],G), %4 colonnes adjacentes
 		   length(I1,L1), length(I2,L2), length(I3,L3), length(I4,L4),
 		   L2 is L1+1, L3 is L2+1, L4 is L3+1.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Lecture de la colonne utilisateur %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%colonnes valides
+col(1).
+col(2).
+col(3).
+col(4).
+col(5).
+col(6).
+col(7).
 
-
+lireColonne(C):- nl, write('Colonne : '), repeat,
+		get_char(L),
+        atom_number(L,C),
+        col(C).
