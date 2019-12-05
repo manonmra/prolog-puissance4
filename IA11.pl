096d4186a2f92f211d78df585928b915ad7a5bf9
MRISBOURG <45947066+MRISBOURG@users.noreply.github.com>
2019-12-05 21:54:27 +0100

    New IA (ia3)

--- IA11.pl
+++ IA11.pl
@@ -12,6 +12,16 @@ initialiser([
 	       ['_','_','_','_','_','_'] 
 ]).
 
+initialiser2([
+		   ['_','_','_','_','_','_'],
+	       ['_','_','_','_','_','_'],  
+	       ['_','_','_','_','_','_'],
+	       ['_','_','_','_','X','X'],
+	       ['_','_','_','_','_','O'],
+	       ['_','_','_','_','_','_'],
+	       ['_','_','_','_','_','_'] 
+]).
+
 
 
 % Dans la grille du dessus, chaque ligne correspond à une colonne du jeu de puissance4.
@@ -19,6 +29,8 @@ initialiser([
 % La casse de coordonnée (6;7) dans la grille ci-dessus correspond à la case "tout en bas  à gauche" du jeu de puissance4
 
 puissance4 :- initialiser(G), afficher(G), nl, jouerCoup('X', G).
+puissance3 :- initialiser2(G), premierPion(G,2,'O',_).
+
 
 
 %%%%%%%%%%%%%%%%%%%%%%%
@@ -33,18 +45,52 @@ gameOver(Joueur,G):- victoire(Joueur,G), write('Victoire du joueur '), write(Jou
 gameOver(_,G):- remplie(G), write('Match nul, la grille est pleine!'), !.
 
 jouerCoup(Joueur,G):- joueurSuivant(Joueur, JoueurSuivant), gameOver(JoueurSuivant,G).
+
+%Joueur Humain
 jouerCoup('X',G):-
 			write('Au tour de X de jouer.'),
             lireColonne(C), % entrée utilisateur
             ajouterDansColonne('X', C, G, G2),
 			afficher(G2), nl, %afficher la grille de jeu
             jouerCoup('O', G2). %faire jouer le joueur suivant
+%IA Random
+jouerCoup('NA',G):-
+			write('Au tour de X de jouer.'), nl,
+            repeat,
+			random(1,8,C),
+			ajouterDansColonne('O', C, G, G2),
+			afficher(G2), nl,
+            jouerCoup('X', G2). %faire jouer le joueur suivant
+			
+
+%IA Centre Avec W/L
+jouerCoup('NA',G):- 
+			write('Au tour de O de jouer.'), nl,
+			ia1('O', G, G2, 7),
+			afficher(G2), nl, %afficher la grille de jeu
+            jouerCoup('X', G2). %faire jouer le joueur suivant
+
+%IA Centre
+jouerCoup('NA',G):- 
+			write('Au tour de O de jouer.'), nl,
+			ia2('X', G, G2, 7),
+			afficher(G2), nl, %afficher la grille de jeu
+            jouerCoup('O', G2). %faire jouer le joueur suivant
+
+%IA Centre Dessus
 jouerCoup('O',G):- 
 			write('Au tour de O de jouer.'), nl,
-			%repeat,
-            %random(1,8,C),
-			ia1('O', G, G2, 7),
-            %ajouterDansColonne('O', C, G, G2),
+			ia3('O', G, G2, 7),
+			afficher(G2), nl, %afficher la grille de jeu
+            jouerCoup('X', G2). %faire jouer le joueur suivant
+
+%IA Min-Max
+jouerCoup('NA',G):- 
+			write('Au tour de O de jouer.'), nl,
+            creerUneListeTest(Liste),
+            nl, write('Liste : '), write(Liste),nl,
+			evaluerEtChoisir(Liste, G, 0, 1, (0,-1001), (Coup,Valeur), 'O'),nl,
+            ajouterDansColonne('O', Coup, G, G2),
 			afficher(G2), nl, %afficher la grille de jeu
             jouerCoup('X', G2). %faire jouer le joueur suivant
 
@@ -149,7 +195,7 @@ gagneEnUnCoup(S, G, C, C3):-
                             (C2 is C-1, gagneEnUnCoup(S, G, C2, C))
                         ),!. */
 
-gagneEnUnCoup(S, G, 0, C2):- C2 is 0, !.
+gagneEnUnCoup(_, _, 0, C2):- C2 is 0, !.
 gagneEnUnCoup(S, G, C, C2):- ajouterDansColonne(S,C,G,G2), victoire(S,G2), C2 is C, !.
 gagneEnUnCoup(S, G, C, C2):- C3 is C-1, gagneEnUnCoup(S, G, C3, C2).
 
@@ -171,3 +217,223 @@ ia1(S, G, G2, _):-ajouterDansColonne(S, 6, G, G2).
 ia1(S, G, G2, _):-ajouterDansColonne(S, 2, G, G2).
 ia1(S, G, G2, _):-ajouterDansColonne(S, 7, G, G2).
 ia1(S, G, G2, _):-ajouterDansColonne(S, 1, G, G2).
+
+
+
+
+ia2(S, G, G2, _):-ajouterDansColonne(S, 4, G, G2).
+ia2(S, G, G2, _):-ajouterDansColonne(S, 5, G, G2).
+ia2(S, G, G2, _):-ajouterDansColonne(S, 3, G, G2).
+ia2(S, G, G2, _):-ajouterDansColonne(S, 6, G, G2).
+ia2(S, G, G2, _):-ajouterDansColonne(S, 2, G, G2).
+ia2(S, G, G2, _):-ajouterDansColonne(S, 7, G, G2).
+ia2(S, G, G2, _):-ajouterDansColonne(S, 1, G, G2).
+
+pionDessous(_,0,S,H):- append(_,['_','_','_',S|_],H),!.
+pionDessous(_,0,S,H):- append(_,['_','_',S,S|_],H),!.
+pionDessous([T1|T2],C,S,_):- C2 is C-1, pionDessous(T2,C2,S,T1).
+
+premierPion(_,0,S,H):- append(['_','_','_'],['_','_','_'],H),!.
+%premierPion(_,0,S,H):- H is ['_','_','_','_','_','_'],!.
+premierPion([T1|T2],C,S,_):- C2 is C-1, premierPion(T2,C2,S,T1).
+
+%pionDessous(S, G, C):- trouverColonne(G,C,_,H2), append(_,['_','_','_',S|_],H2),!.
+
+
+%implémentation de l'IA3 (si possible cherche à jouer au milieu de la grille + Jouer au dessus d'un de nos pions)
+ia3(S, G, G2, C):-gagneEnUnCoup(S, G, C, C2), \+ C2 is 0, ajouterDansColonne(S, C2, G, G2).
+ia3(S, G, G2, _):-pionDessous(G,4,S,_), ajouterDansColonne(S, 4, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-pionDessous(G,5,S,_), ajouterDansColonne(S, 5, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-pionDessous(G,3,S,_), ajouterDansColonne(S, 3, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-pionDessous(G,6,S,_), ajouterDansColonne(S, 6, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-pionDessous(G,2,S,_), ajouterDansColonne(S, 2, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-pionDessous(G,7,S,_), ajouterDansColonne(S, 7, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-pionDessous(G,1,S,_), ajouterDansColonne(S, 1, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+
+ia3(S, G, G2, _):-premierPion(G,4,S,_), ajouterDansColonne(S, 4, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-premierPion(G,5,S,_), ajouterDansColonne(S, 5, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-premierPion(G,3,S,_), ajouterDansColonne(S, 3, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-premierPion(G,6,S,_), ajouterDansColonne(S, 6, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-premierPion(G,2,S,_), ajouterDansColonne(S, 2, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-premierPion(G,7,S,_), ajouterDansColonne(S, 7, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-premierPion(G,1,S,_), ajouterDansColonne(S, 1, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+
+
+%si on a pas de pion bien placé
+ia3(S, G, G2, _):-ajouterDansColonne(S, 4, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-ajouterDansColonne(S, 5, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-ajouterDansColonne(S, 3, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-ajouterDansColonne(S, 6, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-ajouterDansColonne(S, 2, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-ajouterDansColonne(S, 7, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+ia3(S, G, G2, _):-ajouterDansColonne(S, 1, G, G2), nePerdPasAuProchainCoup(S, G2,7).
+
+%Si on perd
+ia3(S, G, G2, _):-ajouterDansColonne(S, 4, G, G2).
+ia3(S, G, G2, _):-ajouterDansColonne(S, 5, G, G2).
+ia3(S, G, G2, _):-ajouterDansColonne(S, 3, G, G2).
+ia3(S, G, G2, _):-ajouterDansColonne(S, 6, G, G2).
+ia3(S, G, G2, _):-ajouterDansColonne(S, 2, G, G2).
+ia3(S, G, G2, _):-ajouterDansColonne(S, 7, G, G2).
+ia3(S, G, G2, _):-ajouterDansColonne(S, 1, G, G2).
+
+
+
+
+
+
+
+%%%%%%%%%%%%%
+%% MIN MAX %%
+%%%%%%%%%%%%%
+
+adversaire('O','X'). 
+adversaire('X','O').
+
+% choisir le MeilleurCoup parmi l'ensemble des Coups à partir de la grille courante
+% en utilisant minmax, anticipant de Profondeur niveaux. MaxMin indique si l'on minimise ou maximise couramment. 
+% Record enregistre le meilleur mouvement courant. 
+evaluerEtChoisir([Coup|Coups], Grille, Profondeur, MaxMin, Record, MeilleurCoup, Joueur):- 
+	ajouterDansColonne(Joueur, Coup, Grille, Grille1), % on simule un coup et on stocke la grille hypothétique dans Grille1
+	minmax(Profondeur, Grille1, MaxMin, Coup, Valeur, Joueur), % renvoie si jouer en Coup est une bonne idée
+	%ici, parfois, on finit d'explorer tout un coté de l'arbre
+	maj(Coup, Valeur, Record, Record1),   %% est-ce que jouer en Coup devient notre meilleure idée
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%nl, write('mon record quand je joue à la colonne '),write(Coup), write(' est :'),write(Record1), 
+	evaluerEtChoisir(Coups, Grille, Profondeur, MaxMin, Record1, MeilleurCoup, Joueur). % essayons de jouer autre part
+evaluerEtChoisir([], Grille, Profondeur, MaxMin, Record, Record, Joueur).	 %notre meilleure idée est notre record actuel
+
+minmax(0, Grille, MaxMin, Coup, Valeur, Joueur):-
+	valeur(Grille, Coup, V, Joueur), % permet d'évaluer une grille : résultat dans V
+	Valeur is V*MaxMin.
+	%nl,write('Coup : '),write(Coup),
+	%nl, write('Grille'), write(Grille),
+	%nl,write('Valeur:'),write(Valeur). 
+minmax(Profondeur, Grille, MaxMin, Coup, Valeur, Joueur):-
+	Profondeur>0,
+	%set_of(M, bouger(Grille, M), Coups), % fonction qui renvoie une grille après un coup
+	%ajouterDansColonne(Joueur, Coup, Grille, Grille1),
+	coupsPossibles(Grille, Coups),
+	Profondeur1 is Profondeur-1,
+	MinMax is -MaxMin,
+	adversaire(Joueur,AutreJoueur),
+	evaluerEtChoisir(Coups, Grille, Profondeur1, MinMax, (0,-1001),(Coup, Valeur), AutreJoueur).
+	%write('VALEUR :'), write(Valeur). 
+
+coupsPossibles(Grille1, [1,2,3,4,5,6,7]).
+
+maj(Coup, Valeur, (Coup1, Valeur1), (Coup1, Valeur1)):-
+	Valeur=<Valeur1. 
+maj(Coup, Valeur, (Coup1,Valeur1),(Coup,Valeur)):-
+	Valeur>Valeur1. 
+
+%permet d'évaluer une position(grille)
+valeur(Grille, Coup, V, Joueur):-
+	pointsDePosition(Grille, Coup, Points1, Joueur),
+	adversaire(Joueur, ProchainJoueur),
+	pointsDePosition(Grille, Coup, PointsDeBlocage, ProchainJoueur),
+	V is Points1+PointsDeBlocage/2.
+	%nl,  write('POINTS : '), write(V).
+
+creerUneListeTest([1,2,3,4,5,6,7]).
+testPointsDePosition(X) :- initialiser(Grille), creerUneListeTest(Liste),evaluerEtChoisir(Liste, Grille, 0, 1, (0,-1001), MeilleurCoup, 'O'), nl,write('Meilleur coup : '),write(MeilleurCoup).
+
+pointsDePosition(Grille, Coup, Point, Joueur):-
+	%regarder si on aligne notre nouveau pion avec d'autres pions
+	%regardons vers le bas
+	recupererLaColonneN(Coup, Grille, Col),
+	recupererLesJetonsEnDessous(Col, 0, Joueur, NombreDeJetons),
+	(NombreDeJetons>1->
+		NombreDeJetonsPow is NombreDeJetons-1,
+		pow(10,NombreDeJetonsPow, PointPositionBas);
+		PointPositionBas is 0),
+	%nl,write('point position bas : '), write(PointPositionBas),
+
+	recupererNumeroLigne(Col, 1, NumeroLigne),
+	%Regardons vers la gauche 
+	recupererLesJetonsAGauche(Coup,NumeroLigne,Grille, 0,NombreDeJetonsAGauche, Joueur),
+	(NombreDeJetonsAGauche>0->pow(10,NombreDeJetonsAGauche, PointPositionGauche);PointPositionGauche is 0),
+	%nl,write('point position gauche : '), write(PointPositionGauche),
+
+	%Regardons vers la droite 
+	recupererLesJetonsADroite(Coup,NumeroLigne,Grille, 0,NombreDeJetonsADroite, Joueur),
+	(NombreDeJetonsADroite>0->pow(10,NombreDeJetonsADroite, PointPositionDroite);PointPositionDroite is 0),
+	%nl,write('point position droite : '), write(PointPositionDroite),
+
+	NombreDeJetonsHorizontal is (NombreDeJetonsAGauche+NombreDeJetonsADroite),
+	(NombreDeJetonsHorizontal>0 -> pow(10,NombreDeJetonsHorizontal,NombreDePointsHorizontal); NombreDePointsHorizontal is 0),
+	%nl,write('Nombre de points horizontal :'), write(NombreDePointsHorizontal),
+
+	Point is PointPositionBas+NombreDePointsHorizontal.
+
+recupererLaColonneN(1,[Y|Z],Y):- !.
+recupererLaColonneN(N,[Y|Z],Col):- N2 is N-1, recupererLaColonneN(N2,Z,Col).
+
+recupererNumeroLigne(['_'|Z], N, NumeroLigne) :- N2 is N+1,recupererNumeroLigne(Z,N2,NumeroLigne). 
+recupererNumeroLigne([Y|Z], N, NumeroLigne) :- NumeroLigne is N, !.
+
+recupererLesJetonsEnDessous([], N, Joueur, Result):- Result is N,!. 
+recupererLesJetonsEnDessous(['_'], N, Joueur, Result):- Result is N,!. % la colonne était vide, on va renvoyer 0
+recupererLesJetonsEnDessous(['_'|Q], N, Joueur, Result):- recupererLesJetonsEnDessous(Q,N, Joueur, Result). % utilisé pour descendre dans la colonne
+recupererLesJetonsEnDessous([H|Q], N, Joueur, Result):- 
+	H \== ('_'), 
+	(H == Joueur-> 
+	N2 is N+1,
+	recupererLesJetonsEnDessous(Q, N2, Joueur, Result)% si l'on souhaite jouer au dessus de quelqu'un
+	; Result is N).
+
+
+%% fonction puissance
+pow(_,0,1).
+pow(B,E,R) :- E > 0,!, E1 is E -1, pow(B,E1,R1), R is B * R1.
+
+recupererLesJetonsAGauche(1, NumeroLigne, Grille, N, NombreDeJetons, Joueur):-NombreDeJetons is N,!.% on sort parce qu'on sort du terrain
+recupererLesJetonsAGauche(NumeroColonne, NumeroLigne, Grille, N, NombreDeJetons, Joueur):-
+	recupererJetonAGauche(NumeroColonne, NumeroLigne, Grille, JetonAGauche),
+	lireJetonDeGauche(JetonAGauche, Joueur, NumeroColonne, NumeroLigne, Grille, N, NombreDeJetons).
+
+
+% jeton, joueur
+lireJetonDeGauche(X, X, NumeroColonne, NumeroLigne, Grille, N, NombreDeJetons) :- 
+	NumColonneDeGauche is NumeroColonne-1,
+	N2 is N+1,
+	recupererLesJetonsAGauche(NumColonneDeGauche, NumeroLigne, Grille, N2, NombreDeJetons, X).
+lireJetonDeGauche('_', X, NumeroColonne, NumeroLigne, Grille, N, NombreDeJetons) :-% on sort parce que c'est vide
+	recupererLesJetonsAGauche(1, NumeroLigne, Grille, N, NombreDeJetons, X).
+lireJetonDeGauche(X, Joueur, NumeroColonne, NumeroLigne, Grille, N, NombreDeJetons):-
+	recupererLesJetonsAGauche(1, NumeroLigne, Grille, N, NombreDeJetons, Joueur). %on sort parce que c'est un jeton de l'autre joueur
+
+
+recupererJetonAGauche(NumeroColonne, NumeroLigne, Grille, Jeton):-
+	NumColonneDeGauche is NumeroColonne-1,
+	recupererLaColonneN(NumColonneDeGauche,Grille, ColonneGauche),
+	recupererJetonLigneN(ColonneGauche, NumeroLigne, Jeton).
+
+%%%%%%%%%%%%%%%A DROITE 
+
+recupererLesJetonsADroite(7, NumeroLigne, Grille, N, NombreDeJetons, Joueur):-NombreDeJetons is N,!.% on sort parce qu'on sort du terrain
+recupererLesJetonsADroite(NumeroColonne, NumeroLigne, Grille, N, NombreDeJetons, Joueur):-
+	recupererJetonADroite(NumeroColonne, NumeroLigne, Grille, JetonADroite),
+	lireJetonDeDroite(JetonADroite, Joueur, NumeroColonne, NumeroLigne, Grille, N, NombreDeJetons).
+
+
+% jeton, joueur
+lireJetonDeDroite(X, X, NumeroColonne, NumeroLigne, Grille, N, NombreDeJetons) :- 
+	NumColonneDeDroite is NumeroColonne+1,
+	N2 is N+1,
+	recupererLesJetonsADroite(NumColonneDeDroite, NumeroLigne, Grille, N2, NombreDeJetons, X).
+lireJetonDeDroite('_', X, NumeroColonne, NumeroLigne, Grille, N, NombreDeJetons) :-
+	recupererLesJetonsAGauche(1, NumeroLigne, Grille, N, NombreDeJetons, X).
+lireJetonDeDroite(X, Joueur, NumeroColonne, NumeroLigne, Grille, N, NombreDeJetons):-
+	recupererLesJetonsADroite(7, NumeroLigne, Grille, N, NombreDeJetons, Joueur). %on sort parce que c'est un jeton de l'autre joueur
+
+
+recupererJetonADroite(NumeroColonne, NumeroLigne, Grille, Jeton):-
+	NumColonneDeDroite is NumeroColonne+1,
+	recupererLaColonneN(NumColonneDeDroite,Grille, ColonneDroite),
+	recupererJetonLigneN(ColonneDroite, NumeroLigne, Jeton).
+
+%%%%%%%%%%%%%%%%%%
+
+
+recupererJetonLigneN([X|Y], 1 , X) :- !.
+recupererJetonLigneN([X|Y], NumeroLigne, Jeton):-NumeroLigne2 is NumeroLigne-1, recupererJetonLigneN(Y, NumeroLigne2, Jeton).
